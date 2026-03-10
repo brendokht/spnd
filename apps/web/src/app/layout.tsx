3f900aa0891b45f443b30c1edf94617040130cd9
Brenden Kohut <103231108+brendokht@users.noreply.github.com>
2026-03-09 20:56:33 -0400

    Add alert dialog, dialog, field, item, tooltip, and badge components

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/header";
 import { AuthProvider } from "@/context/auth";
+import { TooltipProvider } from "@spnd/ui/components/ui/tooltip";
 import "@spnd/ui/globals.css";
 import type { Metadata } from "next";
 
@@ -17,8 +18,10 @@ export default function RootLayout({
     <html lang="en">
       <body className="flex min-h-dvh flex-col bg-white px-4 py-4 text-gray-900 md:px-8">
         <AuthProvider>
-          <Header />
-          <main className="my-8 space-y-4">{children}</main>
+          <TooltipProvider>
+            <Header />
+            <main className="my-8 space-y-4">{children}</main>
+          </TooltipProvider>
         </AuthProvider>
       </body>
     </html>
