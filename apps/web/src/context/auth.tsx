037c5e210e2823f20e269d50476562d41200c4f6
Brenden Kohut <103231108+brendokht@users.noreply.github.com>
2026-03-10 01:19:57 -0400

    Add settings page, updates to auth context and local Supabase config

--- apps/web/src/context/auth.tsx
+++ apps/web/src/context/auth.tsx
@@ -1,46 +1,81 @@
 "use client";
 
-import { createContext, useContext, useEffect, useState } from "react";
-import type { ReactNode } from "react";
-import type { Session, User } from "@supabase/supabase-js";
 import { supabase } from "@/lib/supabase";
+import type { Session, User } from "@supabase/supabase-js";
+import type { ReactNode } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 interface AuthContextType {
   user: User | null;
+  userIdentities: Array<string> | null;
   session: Session | null;
   loading: boolean;
 }
 
 const AuthContext = createContext<AuthContextType>({
   user: null,
+  userIdentities: null,
   session: null,
   loading: true,
 });
 
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
+  const [userIdentities, setUserIdentities] = useState<Array<string>>([]);
   const [session, setSession] = useState<Session | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data }) => {
-      setSession(data.session);
-      setUser(data.session?.user ?? null);
+      // Refresh session if there is a new email, in an attempt to visually show the correct email
+      if (data.session?.user.new_email)
+        supabase.auth.refreshSession().then(({ data }) => {
+          setSession(data.session);
+          setUser(data.session?.user ?? null);
+        });
+      else {
+        setSession(data.session);
+        setUser(data.session?.user ?? null);
+      }
       setLoading(false);
+    });
+
+    supabase.auth.getUserIdentities().then(({ data, error }) => {
+      if (error) {
+        console.error("getUserIdentities Error", error);
+        return;
+      }
+      if (data?.identities) {
+        setUserIdentities(data.identities.map((identity) => identity.provider));
+      }
     });
 
     const {
       data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-      setUser(session?.user ?? null);
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event === "INITIAL_SESSION") {
+        setSession(session);
+        setUser(session?.user ?? null);
+      } else if (event === "SIGNED_IN") {
+        setSession(session);
+        setUser(session?.user ?? null);
+      } else if (event === "SIGNED_OUT") {
+        setSession(null);
+        setUser(null);
+      } else if (event === "USER_UPDATED") {
+        setSession(session);
+        setUser(session?.user ?? null);
+      } else if (event === "TOKEN_REFRESHED") {
+        setSession(session);
+        setUser(session?.user ?? null);
+      }
     });
 
     return () => subscription.unsubscribe();
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, session, loading }}>
+    <AuthContext.Provider value={{ user, userIdentities, session, loading }}>
       {children}
     </AuthContext.Provider>
   );
