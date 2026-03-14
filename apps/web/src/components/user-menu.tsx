f4a0afc376744152eb6e89a39521f9143530bb08
Brenden Kohut <103231108+brendokht@users.noreply.github.com>
2026-03-14 15:50:42 -0400

    Small header and user menu change, add reusable Google component

--- apps/web/src/components/user-menu.tsx
+++ apps/web/src/components/user-menu.tsx
@@ -16,72 +16,58 @@ import {
   DropdownMenuTrigger,
 } from "@spnd/ui/components/ui/dropdown-menu";
 import { User } from "@supabase/supabase-js";
-import { Cog, LogIn, LogOut } from "lucide-react";
+import { Cog, LogOut } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-export default function UserMenu({ user }: { user: User | null }) {
+export default function UserMenu({ user }: { user: User }) {
   const supabase = createClient();
   const router = useRouter();
   return (
     <>
-      {user ? (
-        <DropdownMenu>
-          <DropdownMenuTrigger
-            render={
-              <Button variant="ghost" size="icon" className="rounded-full">
-                <Avatar>
-                  <AvatarImage
-                    src={user.user_metadata["avatar_url"]}
-                    alt="shadcn"
-                  />
-                  <AvatarFallback>
-                    {user.email?.charAt(0).toUpperCase()}
-                  </AvatarFallback>
-                </Avatar>
-              </Button>
-            }
-          />
-          <DropdownMenuContent className="w-40" align="start">
-            <DropdownMenuGroup>
-              <DropdownMenuItem
-                nativeButton={false}
-                render={
-                  <Link href="/settings">
-                    <Cog />
-                    Settings
-                  </Link>
-                }
-              ></DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem
-                onClick={() => {
-                  supabase.auth.signOut();
-                  router.push("/login");
-                }}
-                variant="destructive"
-              >
-                <LogOut />
-                Log out
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      ) : (
-        <Button
-          variant={"outline"}
-          size={"default"}
-          nativeButton={false}
+      <DropdownMenu>
+        <DropdownMenuTrigger
           render={
-            <Link href={"/sign-in"}>
-              <LogIn />
-              Log in
-            </Link>
+            <Button variant="ghost" size="icon" className="rounded-full">
+              <Avatar>
+                <AvatarImage
+                  src={user.user_metadata["avatar_url"]}
+                  alt="shadcn"
+                />
+                <AvatarFallback>
+                  {user.email?.charAt(0).toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+            </Button>
           }
         />
-      )}
+        <DropdownMenuContent className="w-40" align="start">
+          <DropdownMenuGroup>
+            <DropdownMenuItem
+              nativeButton={false}
+              render={
+                <Link href="/settings">
+                  <Cog />
+                  Settings
+                </Link>
+              }
+            ></DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem
+              onClick={() => {
+                supabase.auth.signOut();
+                router.push("/login");
+              }}
+              variant="destructive"
+            >
+              <LogOut />
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </>
   );
 }
